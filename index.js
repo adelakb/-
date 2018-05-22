fa9bd2c31b99087cbf3294166eaa73a0bd013eac
adelakb <38085253+adelakb@users.noreply.github.com>
2018-05-22 04:40:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,32 +4,32 @@ const client = new Discord.Client();
 client.on('message', message => {
     if (message.content.includes('ncode')) {
     if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
-    message.guild.me.setNickname(message.content.replace('ncode ', 'code bunker '));
+    message.guild.me.setNickname(message.content.replace('ncode ', 'code bunker :radio:  '));
 }
 
 });
 
 client.on('message', message => 
 { if (message.content === 'code bunker') {
-message.channel.send(':radio:                   :arrow_up: ');
+message.channel.send('                   :arrow_up: ');
 }
 });
 
 client.on('message', message => 
 { if (message.content === 'bunker code') {
-message.channel.send(':radio:                   :arrow_up: ');
+message.channel.send('                   :arrow_up: ');
 }
 });
 
 client.on('message', message => 
 { if (message.content === 'كود البنكر') {
-message.channel.send(':radio:                   :arrow_up: ');
+message.channel.send('                   :arrow_up: ');
 }
 });
 
 client.on('message', message => 
 { if (message.content === 'رمز البنكر') {
-message.channel.send(':radio:                   :arrow_up: ');
+message.channel.send('                   :arrow_up: ');
 }
 });
 
