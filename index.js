20ccb77054af491ecf78d2bd2100b26c4c839217
adelakb <38085253+adelakb@users.noreply.github.com>
2018-05-22 02:11:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,66 +1,39 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-         client.on('message', message => {
-			    var prefix = ".";
-				const devs = ['413509726735826946' , '' , '' , ''];
-
-    if (message.author.id === client.user.id) return;
-    if (message.guild) {
-   let embed = new Discord.RichEmbed()
-    let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == prefix + 'msg') {
-    if (!args[1]) {
-return;
-}
-        message.guild.members.forEach(m => {
-   if (!devs.includes(message.author.id)) return;
-            var bc = new Discord.RichEmbed()
-            .addField(' hello survivors ', args)
-			.setFooter('Last Day on Earth: Survival', 'https://cdn.discordapp.com/attachments/435973456467591170/446103479442735114/Last_Day_On_Earth_Survival.jpg')
-            .setColor('#ff0000')
-            m.send({embed: bc});
-        });
-    }
-    } else {
-        return;
-    }
-});
-
-const devs = ['413509726735826946' , '290260570374930432' , '406100430578253826' , ''];
-const adminprefix = "+";
 client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
-      
-   else 
-  if (message.content.startsWith(adminprefix + 'code')) {
-  client.user.setActivity(argresult , {type:'WATCHING'});
-      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
-  } 
+    if (message.content.includes('-n')) {
+    if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
+    message.guild.me.setNickname(message.content.replace('n ', 'code bunker '));
+}
+
+});
+
+client.on('message', message => 
+{ if (message.content === 'code bunker') {
+message.channel.send(':radio:                   :arrow_up: ');
+}
+});
+
+client.on('message', message => 
+{ if (message.content === 'bunker code') {
+message.channel.send(':radio:                   :arrow_up: ');
+}
+});
+
+client.on('message', message => 
+{ if (message.content === 'كود البنكر') {
+message.channel.send(':radio:                   :arrow_up: ');
+}
+});
+
+client.on('message', message => 
+{ if (message.content === 'رمز البنكر') {
+message.channel.send(':radio:                   :arrow_up: ');
+}
 });
 
 
-   client.on('message', message => {
-if (message.content.startsWith('ldoeinfo')){
-     let embed = new Discord.RichEmbed()
-  .setColor("random")
-  .setFooter('Last Day on Earth: Survival', 'https://cdn.discordapp.com/attachments/435973456467591170/446103479442735114/Last_Day_On_Earth_Survival.jpg')
-  .addField('OFFICIAL LINKS', ':tm:')
-  .addField('Google Play (for Android users):' , 'https://goo.gl/24XTLu')
-  .addField('Apps Store (for iOS users) :' , 'https://appsto.re/ru/-eMbkb.i')
-  .addField('SUPPORT', 'https://kefirgames.helpshift.com/a/last-day-on-earth/')
-  .addField('FACEBOOK', 'https://www.facebook.com/lastdayonearthgame')
-  .addField('INSTAGRAM', 'https://www.instagram.com/lastdayonearthgame')
-  .addField('TELEGRAM', 'https://t.me/lastdayonearthsurvival')
-  .addField('TWITTER', 'https://twitter.com/lastdaysurvival')
-  .addField('REDDIT', 'https://www.reddit.com/r/LastDayonEarthGame')
-  .addField('DISCORD', 'https://discord.gg/c7Snete')
-
-
-   message.channel.send(embed);
-    }
-});
 
 client.login(process.env.TOKEN)
 
