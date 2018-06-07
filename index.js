bfbb207d95136fc226ec63ecbb733ccf7b3a89e5
adelakb <38085253+adelakb@users.noreply.github.com>
2018-06-08 00:11:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ client.on('message', message => {
     if (message.content.includes('ncode')) {
     if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
     message.guild.me.setNickname(message.content.replace('ncode', 'code bunker '));
-	    message.channel.send(':white_check_mark:');
 }
 });
 
